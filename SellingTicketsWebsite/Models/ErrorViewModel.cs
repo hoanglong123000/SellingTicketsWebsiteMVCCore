3abfd6b5712ad00fff6c55a7977e383b878bdc8c
NguyenHoangLong <hoanglong123000@gmail.com>
2022-12-05 10:23:27 +0700

    Add project files.

---
+++ SellingTicketsWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SellingTicketsWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
